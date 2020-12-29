aa5d5524cd6ed793eb12a2c42b6f1177e4258c83
Gunil <gunil.na1@gmail.com>
2020-12-29 22:19:02 +0100

    day23 move turtle up on up-arrow key

--- day-23/main.py
+++ day-23/main.py
@@ -10,7 +10,12 @@ screen.tracer(0)
 
 player = Player()
 
+screen.listen()
+screen.onkey(player.move, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
