3a0790d885cb4c17b6e6b2dd58d1474cf248cd33
Gunil <gunil.na1@gmail.com>
2020-12-31 20:08:05 +0100

    detect collision and level up

--- day-23/main.py
+++ day-23/main.py
@@ -22,4 +22,11 @@ while game_is_on:
     car_manager.create_car()
     car_manager.move_cars()
 
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+    
+    if player.is_at_finish_line():
+        player.go_to_start()
+
 screen.exitonclick()
--- day-23/player.py
+++ day-23/player.py
@@ -11,8 +11,17 @@ class Player(Turtle):
         super().__init__()
         self.shape('turtle')
         self.penup()
-        self.left(90)
-        self.goto(STARTING_POSITION)
+        self.setheading(90)
+        self.go_to_start()
 
     def move(self):
         self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
